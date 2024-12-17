29d60bd207c14961fa58ca70b6720a1fc9fa1b8d
Rich Green <richard.green@digital.justice.gov.uk>
2024-12-17 10:23:52 +0000

    always include AmazonSSMManagedInstanceCore and variable values

--- main.tf
+++ main.tf
@@ -307,9 +307,9 @@ resource "aws_iam_role" "this" {
 
 # IAM role policy attachment
 resource "aws_iam_role_policy_attachment" "this" {
-  count      = length(var.instance_profile_policies)
+  count      = length(concat(["arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"], var.instance_profile_policies))
   role       = aws_iam_role.this.name
-  policy_arn = var.instance_profile_policies[count.index]
+  policy_arn = element(concat(["arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"], var.instance_profile_policies), count.index)
 }
 
 data "aws_iam_policy_document" "ssm_params_and_secrets" {
